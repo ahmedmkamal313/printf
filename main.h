6f02e0d09546b3f65ab0a80654cb89504ecf1ac4
Magdalina1 <majdalol95@gmail.com>
2023-04-25 06:01:44 -0700

    fix main

--- main.h
+++ main.h
@@ -12,11 +12,11 @@
 #define HASH 4
 #define ZERO 8
 #define NEG 16
-#define PLUS_FLAG (flags & 1)
-#define SPACE_FLAG ((flags >> 1) & 1)
-#define HASH_FLAG ((flags >> 2) & 1)
-#define ZERO_FLAG ((flags >> 3) & 1)
-#define NEG_FLAG ((flags >> 4) & 1)
+#define PLUS_FLAG (format_flags & 1)
+#define SPACE_FLAG ((format_flags >> 1) & 1)
+#define HASH_FLAG ((format_flags >> 2) & 1)
+#define ZERO_FLAG ((format_flags >> 3) & 1)
+#define NEG_FLAG ((format_flags >> 4) & 1)
 
 /* Length Modifier Macros */
 #define SHORT 1
